9857eaae16404ae73a6b1fb98f44208f7ffac85a
PaulMelloy <paul@melloy.com.au>
2024-10-07 09:51:33 +1000

    add more line documentation

--- R/set_API_key.R
+++ R/set_API_key.R
@@ -11,13 +11,19 @@
 #' }
 #'
 set_API_key<- function(email){
+  # set SILO api key as a system variable.
+  Sys.setenv(SILO_API_KEY = email)
 
   if(isFALSE(file.exists(".Renviron"))){
     cat("SILO_API_KEY='", email,"'","\n",sep = "",file = ".Renviron",append = TRUE)
+    warning("R session may need to be restarted to take effect")
+
   }else{
+    # Read .Renviron file if it already exists
     renvir <- file(".Renviron",open = "r")
     renv_contents <- readLines(renvir)
 
+    # if API key already exists exit
     if(any(grepl("SILO_API_KEY=",renv_contents))) return(NULL)
 
     # if empty file pretend there is 1 empty line so no error on logical evaluation
@@ -26,11 +32,11 @@ set_API_key<- function(email){
     # if the last row is not an empty line add new line before key
     if(renv_contents[length(renv_contents)] == ""){
       cat("SILO_API_KEY='", email,"'","\n",sep = "",file = ".Renviron",append = TRUE)
-      close(renvir)
     }else{
       cat("\nSILO_API_KEY='", email,"'","\n",sep = "",file = ".Renviron",append = TRUE)
-      close(renvir)
     }
+    close(renvir)
+    warning("R session may need to be restarted to take effect")
   }
   return(NULL)
 }
